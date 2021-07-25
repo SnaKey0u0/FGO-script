dd10b44cfb66f3b421e918a24807291e27fecc79
SnaKey0u0 <48859497+SnaKey0u0@users.noreply.github.com>
2021-07-25 23:02:21 +0800

    modify something unimportant

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ from utils.mouse_clicker import set_config as set_mouse_clicker
 config_data = {}
 
 
-def gogo(info_obj):
+def load_and_set():
     global config_data
     config_data = load_config()
     set_executor(config_data)
     set_monitor(config_data)
     set_mouse_clicker(config_data)
+
+
+def gogo(info_obj):
     start_playing(info_obj)
 
 
@@ -22,6 +25,10 @@ def testshot():
 
 
 if __name__ == '__main__':
+
+    # 匯入設定檔
+    load_and_set()
+
     # 基本設定
     loop = 2
 
@@ -31,12 +38,17 @@ if __name__ == '__main__':
     use_server = "CBA"
     instructions = list()
 
-    # [1,2]             => 從者1施放2技(不用選擇目標)
-    # [1,2,3]           => 從者1施放2技給從者3
-    # ["CLOTH", 1]      => 使用衣服技能1(不用選擇目標)
-    # ["CLOTH", 1, 2]   => 衣服技能1施放給從者2
-    # ["CLOTH", 3, 2, 6]=> 換人禮裝專用，從者2換從者6
-    # ["ATTACK", 1]     => 開始戰鬥，從者1施放寶具(僅能1人施放)
+    ######################################################################
+    ######################################################################
+    # 指令說明
+    # EX: [1, 2]            => 從者1施放2技(不用選擇目標)
+    # EX: [1, 2, 3]         => 從者1施放2技給從者3
+    # EX: ["CLOTH", 1]      => 使用衣服技能1(不用選擇目標)
+    # EX: ["CLOTH", 1, 2]   => 衣服技能1施放給從者2
+    # EX: ["CLOTH", 3, 2, 6]=> 換人禮裝專用，從者2換從者6
+    # EX: ["ATTACK", 1]     => 開始戰鬥，從者1施放寶具(僅能1人施放)
+    ######################################################################
+    ######################################################################
 
     # 雙CBA不換人(2003)
     # wave_1 = [[2, 1, 1], [3, 1, 1], ["CLOTH", 3, 1], ["ATTACK", 1]]
@@ -52,11 +64,12 @@ if __name__ == '__main__':
     instructions.append(wave_2)
     instructions.append(wave_3)
 
-    # 開始腳本
     info_obj = {"loop": loop,
                 "team": use_team,
                 "class": use_class,
                 "server": use_server,
                 "instructions": instructions}
+
+    # 開始腳本
     gogo(info_obj)
     # testshot()
