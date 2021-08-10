cc4cfab7b8a0c8e7f2e5241ded579ff29dbcd5ef
SnaKey0u0 <48859497+SnaKey0u0@users.noreply.github.com>
2021-08-10 23:35:54 +0800

    增加部分try catch

--- utils/executor.py
+++ utils/executor.py
@@ -15,63 +15,65 @@ def print_config():
 
 
 def start_playing(info_obj):
-    for i in range(info_obj["loop"]):
-        print("loop: "+str(i+1))
-        first_enter = False
-        if not grab_screen_and_click("select_episode1"):
-            first_enter = True
-            if not grab_screen_and_click("select_episode2"):
-                print("opps! something went wrong, script stop!")
-                return
-        time.sleep(1)
-        if not eat_apple(info_obj["apples"]):
-            return
-        first_refresh = True
-        while not grab_screen_and_click(info_obj["server"]):
-            if first_refresh:
-                first_refresh = False
-            else:
-                time.sleep(10)
-            grab_screen_and_click("refresh")
-            time.sleep(1)
-            grab_screen_and_click("yes")
-            time.sleep(4)
-        time.sleep(2)
-        select_team(info_obj["team"])
-        # 進關前倒數
-        time.sleep(3)
-        grab_screen_and_click("start_episode")
-        for wave in info_obj["instructions"]:
-            if not wait_until("wave"):
-                return
-            for step in wave:
-                print("exe ins")
-                # 特殊指令
-                if isinstance(step[0], str):
-                    if step[0] == "CLOTH":
-                        use_cloth(step[1:])
-                    elif step[0] == "ATTACK":
-                        use_ult(step[1:])
-                        break
-                else:
-                    pos = config_data["skill"+str(step[0])+str(step[1])]
-                    click(pos[0], pos[1])
-                    time.sleep(1)
-                    # 指定技
-                    if (len(step) == 3):
-                        pos = config_data["select"+str(step[2])]
-                        click(pos[0], pos[1])
-                    time.sleep(4)
-        if not wait_until("click_screen"):
-            return
-        ending_game()
-        time.sleep(10)
-        if first_enter:
+    try:
+        for i in range(info_obj["loop"]):
+            print("loop: "+str(i+1))
             first_enter = False
-            grab_screen_and_click("click_screen")
-            time.sleep(4)
-    print("finished")
-
+            if not grab_screen_and_click("select_episode1"):
+                first_enter = True
+                if not grab_screen_and_click("select_episode2"):
+                    print("opps! something went wrong, script stop!")
+                    return
+            time.sleep(1)
+            if not eat_apple(info_obj["apples"]):
+                return
+            first_refresh = True
+            while not grab_screen_and_click(info_obj["server"]):
+                if first_refresh:
+                    first_refresh = False
+                else:
+                    time.sleep(10)
+                grab_screen_and_click("refresh")
+                time.sleep(1)
+                grab_screen_and_click("yes")
+                time.sleep(4)
+            time.sleep(2)
+            select_team(info_obj["team"])
+            # 進關前倒數
+            time.sleep(3)
+            grab_screen_and_click("start_episode")
+            for wave in info_obj["instructions"]:
+                if not wait_until("wave"):
+                    return
+                for step in wave:
+                    print("exe ins")
+                    # 特殊指令
+                    if isinstance(step[0], str):
+                        if step[0] == "CLOTH":
+                            use_cloth(step[1:])
+                        elif step[0] == "ATTACK":
+                            use_ult(step[1:])
+                            break
+                    else:
+                        pos = config_data["skill"+str(step[0])+str(step[1])]
+                        click(pos[0], pos[1])
+                        time.sleep(1)
+                        # 指定技
+                        if (len(step) == 3):
+                            pos = config_data["select"+str(step[2])]
+                            click(pos[0], pos[1])
+                        time.sleep(4)
+            if not wait_until("click_screen"):
+                return
+            ending_game()
+            time.sleep(10)
+            if first_enter:
+                first_enter = False
+                grab_screen_and_click("click_screen")
+                time.sleep(4)
+        print("finished")
+    except:
+        return
 
 def use_cloth(step):
     pos = config_data["cloth"]
